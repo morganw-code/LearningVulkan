359ec56ebf93fd133408c8dab3811be24f175323
Morgan Webb <mnbwebb2233@gmail.com>
2020-05-03 17:34:59 +1000

    Base code for creating GLFWwindow

--- main.cpp
+++ main.cpp
@@ -1,33 +1,59 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-
-#define GLM_FORCE_RADIANS
-#define GML_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
 #include <iostream>
+#include <stdexcept>
+// For EXIT_SUCCESS and EXIT_FAILURE macro
+#include <cstdlib>
 
-int main() {
-    glfwInit();
+class Application {
+public:
+    const int WIDTH = 800;
+    const int HEIGHT = 600;
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
     }
 
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    return 0;
+private:
+    GLFWwindow* window;
+    
+    void initWindow() {
+        glfwInit();
+        // Tell GLFW to not create an OpenGL context (hint, value)
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        // Disable window resize
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        // Initialize GLFWwindow (GLFWmonitor param is used for optionally opening window on a specific monitor, and GLFWwindow share param is only relevant to OpenGL)
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
+    void initVulkan() { }
+
+    void mainLoop() {
+        while(!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        // cleanup glfw
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+};
+
+int main() {
+    Application app;
+
+    try {
+        app.run();
+    } catch(const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
